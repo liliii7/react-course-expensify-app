fcdfcf62c5cbc801d834d23447145f0f2544e865
liliii7 <lidiaquinones2005@gmail.com>
2024-08-27 13:57:19 -0400

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,74 @@
-const path = require("path");
+const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
-  const CSSExtract = new ExtractTextPlugin("styles.css");
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
-    entry: "./src/app.js",
+    entry: './src/app.js',
     output: {
-      path: path.join(__dirname, "public"),
-      filename: "bundle.js",
-    },
-    stats: {
-      modules: true,
-      maxModules: Infinity,
-      exclude: undefined,
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js',
     },
     module: {
       rules: [
         {
-          loader: "babel-loader",
           test: /\.js$/,
+          loader: 'babel-loader',
           exclude: /node_modules/,
         },
         {
           test: /\.s?css$/,
           use: CSSExtract.extract({
-            use: [{
-              loader: 'css-loader',
-              options: {
-                sourceMap: true
-              }
-            },{
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true
-            }}
-            ]
-          })
-        }]
-    },
-    devtool: isProduction ? 'source-map' : 'inline-source-map',
-    devServer: {
-      contentBase: path.join(__dirname, "public"),
-      historyApiFallback: true
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true,
+                },
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true,
+                },
+              },
+            ],
+          }),
+        },
+      ],
     },
     plugins: [
       CSSExtract,
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify(isProduction ? 'production' : 'development'),
       }),
-      new webpack.IgnorePlugin({
-        resourceRegExp: /react-router-dom/,
-        contextRegExp: /node_modules/
-      }),
-      ...(isProduction ? [new UglifyJsPlugin({
-        uglifyOptions: {
-          compress: {
-            drop_console: true,
-          },
-          output: {
-            comments: false,
-          },
-        },
-        sourceMap: true,
-
-      })] : []),
+      ...(isProduction
+        ? [
+            new UglifyJsPlugin({
+              uglifyOptions: {
+                compress: {
+                  drop_console: true,
+                },
+                output: {
+                  comments: false,
+                },
+              },
+              sourceMap: true,
+            }),
+          ]
+        : []),
     ],
-  }
-}
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+    },
+    externals: {
+      'react-router-dom': 'commonjs react-router-dom',
+    },
+  };
+};
