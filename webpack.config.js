39dc02b91dfcb2f06a2c66dc3998b8dfa8a849ef
liliii7 <lidiaquinones2005@gmail.com>
2024-08-30 14:42:39 -0400

    Setup test database env

--- webpack.config.js
+++ webpack.config.js
@@ -47,8 +47,8 @@ module.exports = (env, argv) => {
         "process.env.FIREBASE_DATABASE_URL": JSON.stringify(process.env.FIREBASE_DATABASE_URL),
         "process.env.FIREBASE_PROJECT_ID": JSON.stringify(process.env.FIREBASE_PROJECT_ID),
         "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-        "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(process.env.MESSAGING_SENDER_ID),
-        "process.env.FIREBASE_APP_ID": JSON.stringify(process.env.APP_ID),
+        "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+        "process.env.FIREBASE_APP_ID": JSON.stringify(process.env.FIREBASE_APP_ID),
         "process.env.FIREBASE_MEASUREMENT_ID": JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
       }),
       ...(isProduction
