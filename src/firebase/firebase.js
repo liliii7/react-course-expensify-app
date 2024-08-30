327b9ec66b95aaca94f2d32e135fd3507ce497db
liliii7 <lidiaquinones2005@gmail.com>
2024-08-30 14:36:55 -0400

    Setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,7 +8,7 @@ const config = {
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID,
-  measurementId:process.env.FIREBASE_MEASUREMENT_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(config);
